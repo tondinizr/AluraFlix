79f922fb38233c5e9e8e3b7a7d968d0e9511694b
Welinton Diniz Ribeiro <welinton.dr@gmail.com>
2020-07-30 14:23:52 -0300

    BackEnd

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -24,24 +24,23 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    if (window.location.href.includes("localhost")) {
-      const URL = "http://localhost:8080/categorias";
-      fetch(URL).then(async (respostaDoServer) => {
-        if (respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          console.log(resposta);
-          setListaCategorias(resposta);
-          return;
-        }
-        throw new Error("Não foi possível pegar os dados");
-      });
-    }
+    const URL = window.location.hostname.includes("http://localhost/")
+      ? "http://localhost:8080/categorias"
+      : "https://backend-tonflix.herokuapp.com/categorias";
+    fetch(URL).then(async (respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        const resposta = await respostaDoServer.json();
+        console.log(resposta);
+        setListaCategorias(resposta);
+        return;
+      }
+      throw new Error("Não foi possível pegar os dados");
+    });
   }, []);
 
   return (
     <PageDefault>
       <h1 style={{ color: categoria.cor }}>Cadastro de Categoria</h1>
-
       <form>
         <FormField
           Color={categoria.cor}
@@ -99,33 +98,36 @@ function CadastroCategoria() {
         </DefaultButton>
       </form>
 
-      <table>
-        <tbody>
-          <tr>
-            <td>Nome da categoria</td>
-            <td>Descrição</td>
-            <td>Cor</td>
-          </tr>
-
-          {listaCategorias.map((novaCategoria, indice) => {
-            return (
-              <tr key={novaCategoria.id}>
-                <td>{novaCategoria.nome}</td>
-                <td>{novaCategoria.descricao}</td>
-                <td>
-                  <b
-                    style={{
-                      color: novaCategoria.cor,
-                    }}
-                  >
-                    {novaCategoria.cor}
-                  </b>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {listaCategorias.length > 0 ? (
+        <table>
+          <tbody>
+            <tr>
+              <td>Nome da categoria</td>
+              <td>Descrição</td>
+              <td>Cor</td>
+            </tr>
+            {listaCategorias.map((novaCategoria, indice) => {
+              return (
+                <tr key={novaCategoria.id}>
+                  <td>{novaCategoria.nome}</td>
+                  <td>{novaCategoria.descricao}</td>
+                  <td>
+                    <b
+                      style={{
+                        color: novaCategoria.cor,
+                      }}
+                    >
+                      {novaCategoria.cor}
+                    </b>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <div>Carregando...</div>
+      )}
 
       <Link to="/">Ir para home</Link>
     </PageDefault>
